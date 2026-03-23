6da1a2cdfd97c1668453390b21a58f5d5bc6cc0e
palkondakumar <palakondakumar519@gmail.com>
2026-03-23 15:04:31 +0530

    dom_crud

--- student.js
+++ student.js
@@ -1,7 +1,5 @@
-
-
-
 // let allUsers = [];
+// let editIndex = 1
 // function addUser() {
 //     let user = {
 //         studentname: document.getElementById("studentname").value,
@@ -9,8 +7,14 @@
 
 
 //     };
-//     allUsers.push(user)
-//     console.log(allUsers);
+//     if (user.studentname === "") return;
+//     if (editIndex === 1) {
+//         allUsers.push(user)
+//     }
+//     else {
+//         allUsers[editIndex] = user;
+//         editIndex = 1;
+//     }
 //     displayUsers();
 //     clearForm();
 // }
@@ -20,85 +24,26 @@
 // }
 // function displayUsers() {
 //     let tbody = document.querySelector("tbody");
-//     tbody.innerHTML = " ";
-//     allUsers.forEach(function (user) {
+//     tbody.innerHTML = "";
+//     allUsers.forEach(function (user, index) {
 //         let mytr = document.createElement("tr");
 //         mytr.innerHTML = `<td> ${user.studentname}</td>
 //        <td> ${user.roll}</td>
-//        <td><button>edit</button></td>
-//        <td> <button>delete</button></td>
+//          <td><button onclick="editUser(${index})">Edit</button></td>
+//             <td><button onclick="deleteUser(${index})">Delete</button></td>
 //        `;
 //         tbody.append(mytr)
 //     });
 // }
 
-
-
-
-
-
-let allUsers = [];
-let editIndex = 1
-function addUser() {
-    let user = {
-        studentname: document.getElementById("studentname").value,
-        roll: document.getElementById("roll").value,
-
-
-    };
-    if (user.studentname === "") return;
-    if (editIndex === 1) {
-        allUsers.push(user)
-    }
-    else {
-        allUsers[editIndex] = user;
-        editIndex = 1;
-    }
-    displayUsers();
-    clearForm();
-}
-function clearForm() {
-    document.getElementById("studentname").value = "";
-    document.getElementById("roll").value = "";
-}
-function displayUsers() {
-    let tbody = document.querySelector("tbody");
-    tbody.innerHTML = "";
-    allUsers.forEach(function (user, index) {
-        let mytr = document.createElement("tr");
-        mytr.innerHTML = `<td> ${user.studentname}</td>
-       <td> ${user.roll}</td>
-         <td><button onclick="editUser(${index})">Edit</button></td>
-            <td><button onclick="deleteUser(${index})">Delete</button></td>
-       `;
-        tbody.append(mytr)
-    });
-}
-
-function editUser(index) {
-    document.getElementById("studentname").value = allUsers[index].studentname;
-    document.getElementById("roll").value = allUsers[index].roll;
-    editIndex = index;
-}
+// function editUser(index) {
+//     document.getElementById("studentname").value = allUsers[index].studentname;
+//     document.getElementById("roll").value = allUsers[index].roll;
+//     editIndex = index;
+// }
 
 
 // function deleteUser(index) {
 //     allUsers.splice(index, 1);
 //     displayUsers();
 // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
