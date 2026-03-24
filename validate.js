c7eafe02a8259e4e6a7932ee644beced48b99f0a
palkondakumar <palakondakumar519@gmail.com>
2026-03-24 11:36:08 +0530

    edit_delete_crud form

--- validate.js
+++ validate.js
@@ -2,13 +2,22 @@
 
 
 let allUsers = [];
+editIndex=-1;
 function addUser() {
     let user = {
         name: document.getElementById("name").value,
         email: document.getElementById("email").value,
         password: document.getElementById("password").value
     };
+      if(user.name ==="")return;
+      if(editIndex === -1)
+      {
     allUsers.push(user);
+      }
+      else{
+        allUsers[editIndex]=user;
+        editIndex =-1;
+      }
     console.log(allUsers)
     displayUsers();
     clearForm();
@@ -24,14 +33,27 @@ function clearForm() {
 function displayUsers() {
     let tbody = document.querySelector("tbody");
     tbody.innerHTML = "";
-    allUsers.forEach(function (user) {
+    allUsers.forEach(function (user, index) {
 
         let mytr = document.createElement("tr");
         mytr.innerHTML = `<td>${user.name}</td>
                          <td>${user.email}</td>
                          <td>${user.password}</td>
-                       <td><button>edit</button></td>
-                      <td><button>delete</button></td>`;
+                       <td><button onclick="editUser(${index})">edit</button></td>
+                      <td><button onclick="deleteUser(${index})">delete</button></td>`;
         tbody.append(mytr);
     });
+}
+function editUser(index)
+{
+document.getElementById("name").value=allUsers[index].name
+document.getElementById("email").value=allUsers[index].email
+document.getElementById("password").value=allUsers[index].password
+editIndex=index;
+}
+
+function deleteUser(index)
+{
+    allUsers.splice(index, 1);
+    displayUsers();
 }
